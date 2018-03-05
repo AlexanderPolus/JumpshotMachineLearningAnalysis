87e976377578821d93ea3754258378548d97a4cf
AlexanderPolus <alex.polus13@gmail.com>
2018-03-05 17:43:04 -0500

    Create jumpshot.py

---
+++ jumpshot.py
@@ -0,0 +1,5 @@
+#The Following Code is the intellectual property of Alexander Polus, Ryan Parsley, and Connor VanMeter
+#Project Commencement date: 3.5.18
+#
+#Preconditions: A set of training images, followed by test images
+#Postconditions: A trained TensorFlow that will determine the status of the test images: 'miss' or 'make'
